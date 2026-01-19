e44dba6b71c664ea116418cd09abbe89b87c5b3b
VladRud <vru@nemesiscorporation.com>
2026-01-19 23:49:17 +0100

    Prepare for github-pages

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/horse-rasing-game/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
